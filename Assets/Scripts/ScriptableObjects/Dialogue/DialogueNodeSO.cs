07764075dcea62d555faaad0a51662586f814aea
ccandy <ccunandy2001@gmail.com>
2025-06-17 18:12:58 +0800

    Add namespace

--- Assets/Scripts/ScriptableObjects/Dialogue/DialogueNodeSO.cs
+++ Assets/Scripts/ScriptableObjects/Dialogue/DialogueNodeSO.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//对话节点
-[CreateAssetMenu(menuName = "JianAdventure/DialogueNodeSO", fileName = "DialogueNodeSO")]
-public class DialogueNodeSO : ScriptableObject
+
+namespace ProjectJian.SOs
 {
-    public string NodeID;
-    public Sprite Portrait;
-    [TextArea] public string Text;
-    public List<DialogueOptionSO> Options;
+    [CreateAssetMenu(menuName = "JianAdventure/DialogueNodeSO", fileName = "DialogueNodeSO")]
+    public class DialogueNodeSO : ScriptableObject
+    {
+        public string NodeID;
+        public Sprite Portrait;
+        [TextArea] public string Text;
+        public List<DialogueOptionSO> Options;
+    }
 }
+//对话节点
+
--- Assets/Scripts/ScriptableObjects/Dialogue/DialogueOptionSO.cs
+++ Assets/Scripts/ScriptableObjects/Dialogue/DialogueOptionSO.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "JianAdventure/DialogueOptionSO", fileName = "DialogueOptionSO")]
-public class DialogueOptionSO : ScriptableObject
+
+namespace ProjectJian.SOs
 {
-    [TextArea] public string OptionText;
-    public string NextNodeID;
-    // 可在 Inspector 绑定效果脚本或配置字段
+    [CreateAssetMenu(menuName = "JianAdventure/DialogueOptionSO", fileName = "DialogueOptionSO")]
+    public class DialogueOptionSO : ScriptableObject
+    {
+        [TextArea] public string OptionText;
+        public string NextNodeID;
+        // 可在 Inspector 绑定效果脚本或配置字段
+    }
 }
