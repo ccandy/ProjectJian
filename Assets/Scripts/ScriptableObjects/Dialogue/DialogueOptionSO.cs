23531d6d8c5b26c378dc84c39ab511ee107cae91
ccandy <ccunandy2001@gmail.com>
2025-06-19 18:13:57 +0800

    Update DialogueOptionSO add ConsequenceID

--- Assets/Scripts/ScriptableObjects/Dialogue/DialogueOptionSO.cs
+++ Assets/Scripts/ScriptableObjects/Dialogue/DialogueOptionSO.cs
@@ -8,8 +8,15 @@ namespace ProjectJian.SOs
     [CreateAssetMenu(menuName = "JianAdventure/DialogueOptionSO", fileName = "DialogueOptionSO")]
     public class DialogueOptionSO : ScriptableObject
     {
-        [TextArea] public string OptionText;
+        [TextArea]
+        public string OptionText;
+
+        [Tooltip("对话结束后要跳转到的下一个节点 ID")]
         public string NextNodeID;
-        // 可在 Inspector 绑定效果脚本或配置字段
+
+        [Header("Option Effects")]
+        [Tooltip("在 Inspector 中指定选项触发的额外事件 ID 或方法")]
+        public string ConsequenceID;
+
     }
 }
