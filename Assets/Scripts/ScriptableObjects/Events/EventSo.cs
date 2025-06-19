53468e0958c97628b506e7928cc1145bbf89f8df
ccandy <ccunandy2001@gmail.com>
2025-06-19 18:14:09 +0800

    Add JJEventTriggerType

--- Assets/Scripts/ScriptableObjects/Events/EventSo.cs
+++ Assets/Scripts/ScriptableObjects/Events/EventSo.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 //事件 (SO 存配置)
-namespace ProjectJian.SOs
+namespace JianAdventure.SOs
 {
+    public enum JJEventTriggerType
+    {
+        None,
+        EnergyBelow,
+        MoneyBelow,
+        HealthBelow,
+        DiseaseAbove
+    }
     [CreateAssetMenu(menuName = "JianAdventure/EventSO", fileName = "EventSO")]
     public class EventSO : ScriptableObject
     {
         public string EventID;
         public string Description;
-        // 可扩展字段：依赖条件、概率、冷却
+        public JJEventTriggerType TriggerType;
+        public int Threshold;
+        public bool OneTime = true;
     }
 }
 
