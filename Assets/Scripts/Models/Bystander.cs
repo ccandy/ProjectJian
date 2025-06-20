e1d3ac8317286fc8b754ae70e4b613d671c6673b
ccandy <ccunandy2001@gmail.com>
2025-06-20 16:55:38 +0800

    add bystander

---
+++ Assets/Scripts/Models/Bystander.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using JianAdventure.SOs;
+using UnityEngine;
+
+namespace JianAdventure.Models
+{
+    public class Bystander
+    {
+        public BystanderSO Data { get; private set; }
+        public Bystander(BystanderSO so)
+        {
+            Data = so;
+        }
+    }
+
+}
+    
