63a4f46e51d0b088b373bc6c119dd450573b0891
ccandy <ccunandy2001@gmail.com>
2025-06-21 21:28:55 +0800

    Update LocationSO

--- Assets/Scripts/ScriptableObjects/Map/LocationSO.cs
+++ Assets/Scripts/ScriptableObjects/Map/LocationSO.cs
@@ -11,16 +11,23 @@ namespace JianAdventure.SOs
         public int startHour, startMinute;
         public int endHour, endMinute;
 
-        public bool IsOpen() => TimeSystem.Instance.IsWithinTime(startHour, startMinute, endHour, endMinute);
+        public bool IsOpen() =>
+            TimeSystem.Instance.IsWithinTime(startHour, startMinute, endHour, endMinute);
     }
-    
+
     [CreateAssetMenu(menuName = "JianAdventure/LocationSO", fileName = "LocationSO")]
     public class LocationSO : ScriptableObject
     {
+        [Header("基础信息")]
         public string LocationName;
+        public Sprite Icon;                      // 地点图标，用于按钮展示
         public Vector2 Coordinates;
-        public TimeWindow OpenHours;      // 地点开放时间段
-        public List<NPCSO> AvailableNPCs; // 在此地点可能出现的 NPC
+
+        [Header("开放时间")]
+        public TimeWindow OpenHours;             // 地点在此时间段开放
+
+        [Header("可见 NPC")]
+        public List<NPCSO> AvailableNPCs;        // 在此地点、此时间可能出现的 NPC 列表
     }
 }
     
