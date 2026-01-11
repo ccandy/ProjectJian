78f2713d8bbcba00a30086dd1c4382c7664394ee
ccandy <ccunandy2001@gmail.com>
2026-01-10 22:48:19 -0800

    add PlayerStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerStats
+{
+    [Range(0, 100)] public int energy = 60;   // 精力
+    [Range(0, 100)] public int health = 60;   // 健康
+    [Range(0, 100)] public int family = 60;   // 家庭关系
+    public int money = 0;                     // 金钱（可负）
+
+    public int day = 1;                       // 天数计数
+}
+
+[System.Serializable]
+public class StatDelta
+{
+    public int energyDelta;
+    public int healthDelta;
+    public int familyDelta;
+    public int moneyDelta;
+
+    public StatDelta(int e, int h, int f, int m)
+    {
+        energyDelta = e;
+        healthDelta = h;
+        familyDelta = f;
+        moneyDelta = m;
+    }
+}
